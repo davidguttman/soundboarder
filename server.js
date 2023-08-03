8290e58b49b47a41093ee0fc6dff270d1857c0f9
David Guttman <david@davidguttman.com>
2023-08-03 08:18:22 -0700

    feat: add start page, unify style

--- server.js
+++ server.js
@@ -13,7 +13,42 @@ app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
+app.get('/send', handleSend)
+app.get('/receive', handleReceive)
+app.post('/play', handlePlay)
+app.get('/listen', handleListen)
+
+function handlePlay (req, res) {
+  const sound = req.body.sound
+  console.log('play', sound)
+  if (!sound) return res.status(400).send('Missing sound query parameter')
+
+  emitter.emit('play', sound)
+}
+
+function handleListen (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    Connection: 'keep-alive'
+  })
+
+  const onPlay = sound => res.write(`data: ${JSON.stringify(sound)}\n\n`)
+  emitter.on('play', onPlay)
+
+  res.on('close', function () {
+    emitter.off('play', onPlay)
+  })
+}
+
+function handleSend (req, res) {
+  handleRequest('send.html.ejs', req, res)
+}
+
+function handleReceive (req, res) {
+  handleRequest('receive.html.ejs', req, res)
+}
+
+function handleRequest (viewName, req, res) {
   s3.list('misc/soundboard', (err, files) => {
     if (err) return res.status(500).send(err)
 
@@ -30,32 +65,9 @@ app.get('/', (req, res) => {
     const columns = 4
     const rows = Math.ceil(nSounds / columns)
 
-    const view = req.query.send ? 'send.html.ejs' : 'index.html.ejs'
-    res.render(view, { sounds, columns, rows })
+    res.render(viewName, { sounds, columns, rows })
   })
-})
-
-app.post('/play', (req, res) => {
-  const sound = req.body.sound
-  console.log('play', sound)
-  if (!sound) return res.status(400).send('Missing sound query parameter')
-
-  emitter.emit('play', sound)
-})
-
-app.get('/listen', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Connection': 'keep-alive'
-  })
-
-  const onPlay = sound => res.write(`data: ${JSON.stringify(sound)}\n\n`)
-  emitter.on('play', onPlay)
-
-  res.on('close', function () {
-    emitter.off('play', onPlay)
-  })
-})
+}
 
 app.listen(PORT, () => {
   console.log('App is listening on port ' + PORT)
