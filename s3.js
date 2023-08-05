bf4610b512a550b0e6f0c456b7fda92a107ea382
David Guttman <david@davidguttman.com>
2023-08-05 14:27:38 -0700

    style: make standard happy

--- s3.js
+++ s3.js
@@ -1,41 +1,37 @@
-var knox = require('knox')
-var xtend = require('xtend')
-var through2 = require('through2')
-var AsyncCache = require('async-cache')
-
+const knox = require('knox')
+const xtend = require('xtend')
+const through2 = require('through2')
+const AsyncCache = require('async-cache')
 const config = require('./config')
 
 module.exports = createClient(config.s3)
 
 function createClient (config) {
-  var client = knox.createClient(config)
+  const client = knox.createClient(config)
 
-  var dirCache = new AsyncCache({
-    load: function (dir, cb) {
-      getDirTime(client, dir, cb)
-    }
+  const dirCache = new AsyncCache({
+    load: (dir, cb) => getDirTime(client, dir, cb)
   })
 
-  var urlCache = new AsyncCache({
-    load: function (file, cb) {
-      getFileUrl(client, file, cb)
-    }
+  const urlCache = new AsyncCache({
+    load: (file, cb) => getFileUrl(client, file, cb)
   })
 
   return {
-    list: function (dir, cb) {
+    list: (dir, cb) => {
       if (dir[dir.length - 1] !== '/') dir += '/'
       dir = dir.replace(/^\//, '')
-      var opts = { prefix: dir, delimiter: '/' }
+      const opts = { prefix: dir, delimiter: '/' }
 
-      var tr = through2.obj(function (item, enc, cb) {
+      const tr = through2.obj((item, enc, cb) => {
         if (item.Prefix) {
-          dirCache.get(item.Prefix, function (err, time) {
+          dirCache.get(item.Prefix, (err, time) => {
+            if (err) return cb(err)
             item.LastModified = time
             return cb(null, item)
           })
         } else {
-          urlCache.get(item.Key, function (err, url) {
+          urlCache.get(item.Key, (err, url) => {
             if (err) return cb(err)
             item.url = url
             cb(null, item)
@@ -43,23 +39,23 @@ function createClient (config) {
         }
       })
 
-      var buf = []
+      const buf = []
       return s3List(client, opts)
         .pipe(tr)
-        .on('data', function (item) {
+        .on('data', (item) => {
           if (cb) buf.push(item)
         })
-        .on('end', function () {
+        .on('end', () => {
           if (cb) cb(null, buf)
         })
-        .on('error', cb || function () {})
+        .on('error', cb || (() => {}))
     }
   }
 }
 
 function s3List (client, opts, max) {
   opts = opts || {}
-  var stream = through2.obj()
+  const stream = through2.obj()
 
   list(client, opts, stream, max)
   return stream
@@ -67,20 +63,20 @@ function s3List (client, opts, max) {
 
 function list (client, opts, stream, max) {
   if (max) opts['max-keys'] = max
-  var cpOpts = xtend({}, opts)
+  const cpOpts = xtend({}, opts)
 
-  client.list(opts, function (err, data) {
+  client.list(opts, (err, data) => {
     if (err) return stream.emit('error', err)
 
     data.Contents = data.Contents || []
     data.CommonPrefixes = data.CommonPrefixes || []
 
-    var lastKey
-    data.Contents.forEach(function (file) {
+    let lastKey
+    data.Contents.forEach((file) => {
       lastKey = file.Key
       stream.write(file)
     })
-    data.CommonPrefixes.forEach(function (dir) {
+    data.CommonPrefixes.forEach((dir) => {
       stream.write(dir)
     })
 
@@ -94,11 +90,11 @@ function list (client, opts, stream, max) {
 }
 
 function getFileUrl (client, file, cb) {
-  var req = client.request('PUT', encodeURIComponent(file) + '?acl', {
+  const req = client.request('PUT', encodeURIComponent(file) + '?acl', {
     'x-amz-acl': 'public-read'
   })
 
-  req.on('response', function (res) {
+  req.on('response', (res) => {
     if (res.statusCode >= 400) return cb(res.body)
     cb(null, client.http(file))
   })
@@ -107,13 +103,14 @@ function getFileUrl (client, file, cb) {
 }
 
 function getDirTime (client, prefix, cb) {
-  var done = false
+  let done = false
   s3List(client, { prefix: prefix }, 1)
-    .on('data', function (file) {
+    .on('data', (file) => {
       if (done) return
       done = true
       cb(null, file.LastModified)
     })
     .on('error', cb)
-    .on('end', function () {})
+    .on('end', () => {})
 }
+
